1d52684e52fffd7bf6754dc02d3987c24b4471c2
OneEyeX <chedly199@gmail.com>
2025-11-30 00:27:09 +0100

    initial setup : - added dependencies - setup DataBase connection (PostgreSQL)

---
+++ src/main/java/org/example/spothofra/SpotHofraApplication.java
@@ -0,0 +1,13 @@
+package org.example.spothofra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotHofraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotHofraApplication.class, args);
+    }
+
+}
