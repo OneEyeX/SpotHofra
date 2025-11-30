262aaa17d6bea1a20ab009f99c0a0c88477f7c1d
aziz hasnaoui <azizhasnaoui000@gmail.com>
2025-11-30 19:14:51 +0100

    add logic enum + entites

---
+++ src/main/java/org/example/enums/CanalNotification.java
@@ -0,0 +1,6 @@
+package org.example.enums;
+
+public enum CanalNotification {
+    EMAIL,
+    WEB_SOCKET
+}
---
+++ src/main/java/org/example/enums/RoleName.java
@@ -0,0 +1,7 @@
+package org.example.enums;
+
+public enum RoleName {
+    CITOYEN,
+    AGENT_MUNICIPAL,
+    ADMINISTRATEUR
+}
